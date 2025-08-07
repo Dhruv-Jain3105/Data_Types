0abb3b5a5e59d621462a16e8c189aa7f35b735bb
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-07 18:43:01 +0530

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,27 @@
+/*
+Name:-Dhruv Jain
+PRN:-24070123042
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+
+int a=7; //global variable
+
+void func(){
+    extern int a;
+    auto b=10;
+    a=5;
+    cout << "the variable is a is "<< a << " and b is "<< b <<endl;
+}
+
+int main(){
+    func();
+    cout << "The variable is "<< a;
+    return 0;
+}
+/*
+Output:-
+the variable is a is 5 and b is 10
+The variable is 5
+*/
